d5e00982731ad13718621d95303269ec2ec01068
Elizaveta Udodenko <eli.udodenko@gmail.com>
2022-08-06 18:33:19 +0300

    assert fixed

--- test_items.py
+++ test_items.py
@@ -1,12 +1,14 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_add_btn_exist(browser):
     browser.get(link)
-    btn = WebDriverWait(browser, 12).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn-add-to-basket")))
-    assert btn is not None, "Button does not exist."
+    browser.implicitly_wait(10)
+    btn = browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    assert btn, "Button does not exist."
 
